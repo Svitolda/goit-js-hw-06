edbf31bfb134b5c7eed101ce703903230f644d0c
Svitolda <sveta20000203@gmail.com>
2023-03-21 14:00:24 +0100

    08update

--- js/task-08.js
+++ js/task-08.js
@@ -9,25 +9,19 @@ function onFormSubmit(event) {
     console.log(formElements);
     const email = formElements.email;
     const password = formElements.password;
-    // const formData = new FormData(event.currentTarget);
 
     const formData = {
         email,
         password
     };
     console.log(formData)
- 
-    // formData.forEach((name, value) => {
-    //     console.log('onFormSubmit -> name', name);
-    //     console.log('onFormSubmit -> value', value);
-    // });
 
     if (email.value === "" || password.value === "") {
         alert("Fill in the fields of the form");
     } else {
         alert('The form has been submitted!');
-        console.log(`The form has email ${email.value} and password ${password.valeu}`);
+        console.log(`The form has email ${email.value} and password ${password.value}`);
     }
     
-    const from = document.querySelector(".login-form").reset();
+    event.currentTarget.reset();
 }
